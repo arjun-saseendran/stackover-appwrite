474b50b78ece7f3a7fb866b80c2873bbb65384c2
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-16 20:53:17 +0530

    add export name to name.ts

---
+++ src/models/name.ts
@@ -0,0 +1,6 @@
+export const db = 'main-stackover'
+export const questionCollection = 'questions'
+export const answerCollection = 'answers'
+export const commentCollection = 'comments'
+export const voteCollections = 'votes'
+export const questionAttachmentBucket = 'question-attachment'
