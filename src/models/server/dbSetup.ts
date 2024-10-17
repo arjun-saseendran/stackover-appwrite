6456a843f81c57d7aadf00f1f8da67083aef6ab3
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-17 10:28:17 +0530

    finish storage setup

--- src/models/server/dbSetup.ts
+++ src/models/server/dbSetup.ts
@@ -1,2 +1,18 @@
 import { db } from "../name";
+import createQuestionCollection from "./question.collection";
 import createAnswerCollection from './answer.collection'
+import createCommentCollection from "./comment.collection";
+import createVoteCollection from "./vote.collection";
+import {databases} from './config'
+
+export default async function getOrCreateDB(){
+
+    try {
+        
+    } catch (error) {
+        
+    }
+
+}
+
+
