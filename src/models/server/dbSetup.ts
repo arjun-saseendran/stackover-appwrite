107c41a04ef9cab17b6cf40502a7b0ed1336d910
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-17 10:40:22 +0530

    finish dbSetup

--- src/models/server/dbSetup.ts
+++ src/models/server/dbSetup.ts
@@ -1,18 +1,33 @@
 import { db } from "../name";
 import createQuestionCollection from "./question.collection";
-import createAnswerCollection from './answer.collection'
+import createAnswerCollection from "./answer.collection";
 import createCommentCollection from "./comment.collection";
 import createVoteCollection from "./vote.collection";
-import {databases} from './config'
+import { databases } from "./config";
 
-export default async function getOrCreateDB(){
-
+export default async function getOrCreateDB() {
+  try {
+    await databases.get(db);
+    console.log("Database connection");
+  } catch (error) {
     try {
-        
+      await databases.create(db, db);
+
+      console.log("Database created");
+
+      //create collections
+      await Promise.all([
+        createQuestionCollection(),
+        createAnswerCollection(),
+        createCommentCollection(),
+        createVoteCollection(),
+      ]);
+
+      console.log("Collection created");
+      console.log("Database connected");
     } catch (error) {
-        
+      console.log("Error creating databases or collection ", error);
     }
-
+  }
+  return databases;
 }
-
-
