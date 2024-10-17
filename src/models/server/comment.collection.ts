bbec8ac355b2b188c1a87d92606388dafcd621fd
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-17 08:54:53 +0530

    finish comment collection

---
+++ src/models/server/comment.collection.ts
@@ -0,0 +1,42 @@
+import { Permission } from "node-appwrite";
+import { commentCollection, db } from "../name";
+import { databases } from "./config";
+
+export default async function createCommentCollection() {
+  //creating collection
+  await databases.createCollection(db, commentCollection, commentCollection, [
+    Permission.read("any"),
+    Permission.create("users"),
+    Permission.read("users"),
+    Permission.update("users"),
+    Permission.delete("users"),
+  ]);
+  console.log("Comment collection created");
+
+  //creating attributes
+  Promise.all([
+    databases.createStringAttribute(
+      db,
+      commentCollection,
+      "content",
+      10000,
+      true
+    ),
+    databases.createEnumAttribute(
+      db,
+      commentCollection,
+      "type",
+      ["answer", "question"],
+      true
+    ),
+    databases.createStringAttribute(db, commentCollection, "typeId", 50, true),
+    databases.createStringAttribute(
+      db,
+      commentCollection,
+      "authorId",
+      50,
+      true
+    ),
+  ]);
+  console.log("Comment attributes created");
+}
