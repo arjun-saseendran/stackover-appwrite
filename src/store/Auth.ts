8deac18bbb432c3bbc06bd0359d28c56637ebae3
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-18 15:54:32 +0530

    add createAccount

--- src/store/Auth.ts
+++ src/store/Auth.ts
@@ -1,23 +1,28 @@
-import {create} from 'zustand'
-import { immer } from 'zustand/middleware/immer'
-import { persist } from 'zustand/middleware'
-import { AppwriteException, ID, Models } from 'appwrite'
-import { account } from '@/models/client/config'
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+import { persist } from "zustand/middleware";
+import { AppwriteException, ID, Models } from "appwrite";
+import { account } from "@/models/client/config";
 
-
-
-export interface UserPrefs{
-    reputation: number
+export interface UserPrefs {
+  reputation: number;
 }
 
-interface IAuthStore{
-    session: Models.Session | null
-    jwt: string | null
-    user: Models.User<UserPrefs> | null
-    hydrated: boolean
+interface IAuthStore {
+  session: Models.Session | null;
+  jwt: string | null;
+  user: Models.User<UserPrefs> | null;
+  hydrated: boolean;
 
-    setHydrated(): void;
-    verifySession(): Promise<void>;
-    login(email: string, password: string): Promise<{success: boolean; error?: AppwriteException | null}>
-
+  setHydrated(): void;
+  verifySession(): Promise<void>;
+  login(
+    email: string,
+    password: string
+  ): Promise<{ success: boolean; error?: AppwriteException | null }>;
+  createAccount(
+    name: string,
+    email: string,
+    password: string
+  ): Promise<{ success: boolean; error?: AppwriteException | null }>;
 }
