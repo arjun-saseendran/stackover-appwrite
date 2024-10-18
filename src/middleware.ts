7ae15a8bb9e719bcd5bf91d2bb0dcd18378bcf14
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-18 15:03:47 +0530

    add zustand

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,19 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import getOrCreateDB from "./models/server/dbSetup";
+import getOrCreateStorage from "./models/server/storageSetup";
 
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/home", request.url));
+export async function middleware(request: NextRequest) {
+
+    await Promise.all([getOrCreateDB(), getOrCreateStorage()])
+  
+  
+    return NextResponse.next()
 }
 
 export const config = {
   // match all request paths expcept for the ones that starts with:
   // api, _next/static, _next/image, favicon.com
 
-  matcher: "/(?!api|_next/static|_next/image|favicon\\.ico).*",
+  matcher: ["/((?!api|_next/static|_next/image|favicon\\.ico).*)"],
 };
